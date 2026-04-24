4607740f2b565ca29ca510d0f7704f3336099a6c
KetlynLuana <luanaketlyn80@gmail.com>
2026-04-24 11:29:43 -0300

    feat: :sparkles: Atividade

--- src/Aluguel.java
+++ src/Aluguel.java
@@ -1,2 +1,37 @@
 public class Aluguel {
+
+    private String cliente;
+    private String filme;
+    private String dias;
+
+    public Aluguel(String cliente, String filme, String dias) {
+        this.cliente = cliente;
+        this.filme = filme;
+        this.dias = dias;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getFilme() {
+        return filme;
+    }
+
+    public void setFilme(String filme) {
+        this.filme = filme;
+    }
+
+    public String getDias() {
+        return dias;
+    }
+
+    public void setDias(String dias) {
+        this.dias = dias;
+    }
 }
+
--- src/Filme.java
+++ src/Filme.java
@@ -1,2 +1,36 @@
 public class Filme {
+
+    private String titulo;
+    private String genero;
+    private String valorAluguel;
+
+    public Filme(String titulo, String genero, String valorAluguel) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.valorAluguel = valorAluguel;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getValorAluguel() {
+        return valorAluguel;
+    }
+
+    public void setValorAluguel(String valorAluguel) {
+        this.valorAluguel = valorAluguel;
+    }
 }
